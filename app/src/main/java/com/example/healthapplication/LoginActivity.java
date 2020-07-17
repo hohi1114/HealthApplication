81f9f71f71bc12c7fd5dada24a2c46dbc764211c
ohsuz <gl_gd@naver.com>
2020-07-17 15:15:17 +0900

    Initial commit

---
+++ app/src/main/java/com/example/healthapplication/LoginActivity.java
@@ -0,0 +1,31 @@
+package com.example.healthapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        final Button login=(Button)findViewById(R.id.login);
+
+        final TextView register=(TextView)findViewById(R.id.register);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(registerIntent);
+            }
+        });
+
+    }
+}
